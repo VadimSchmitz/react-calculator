5662ef344b849f7218a14f5e2f8c991c5f2146b5
VadimSchmitz <di3mpi3@gmail.com>
2020-08-15 00:42:47 +0200

    css i guess

--- src/App.js
+++ src/App.js
@@ -1,12 +1,73 @@
 import React from 'react';
+import './index.css'
 
 
 
 function App() {
   return (
-    <div>
-     <p>test</p>
+    <div className='wrapper'>
+
+      <div className='flex-row'>
+        <input className='calculator-input ' type="number"></input>
+      </div>
+      <div className='flex-row '>
+        <button className='caculator-button light-grey m-5px'>
+          7
+        </button>
+        <button className='caculator-button light-grey m-5px'>
+          8
+        </button>
+        <button className='caculator-button light-grey m-5px'>
+          9
+        </button>
+        <button className='caculator-button light-blue m-5px'>
+          ÷
+      </button>
+      </div>
+      <div className='flex-row'>
+        <button className='caculator-button light-grey m-5px'>
+          4
+      </button>
+        <button className='caculator-button light-grey m-5px'>
+          5
+      </button>
+        <button className='caculator-button light-grey m-5px'>
+          6
+      </button>
+        <button className='caculator-button light-blue m-5px'>
+          ×
+  </button>
+      </div>
+      <div className='flex-row'>
+        <button className='caculator-button light-grey m-5px'>
+          1
+    </button>
+        <button className='caculator-button light-grey m-5px'>
+          2
+    </button>
+        <button className='caculator-button light-grey m-5px'>
+          3
+    </button>
+        <button className='caculator-button light-blue m-5px'>
+          -
+</button>
+      </div>
+      <div className='flex-row'>
+        <button className='caculator-button light-grey m-5px'>
+          0
+  </button>
+        <button className='caculator-button light-grey m-5px'>
+          .
+  </button>
+        <button className='caculator-button light-grey m-5px'>
+          =
+  </button>
+        <button className='caculator-button light-blue m-5px'>
+          +
+</button>
+      </div>
     </div>
+
   );
 }
 
