77241befdc8ba3e266002a6dcbdcfbef95bb6044
VadimSchmitz <di3mpi3@gmail.com>
2020-08-16 00:34:57 +0200

    learned flexbox

--- src/App.js
+++ src/App.js
@@ -1,78 +1,52 @@
-import React from 'react';
-import './index.css'
-
-
+import React from "react";
+import "./index.css";
+import "./FlexBox.css";
 
 function App() {
   return (
-    <div className='wrapper'>
-      <div className='flex-row'>
-        <input className='calculator-input ' type="number"></input>
-      </div>
-      <div className='flex-row '>
-        <button className='caculator-button light-grey m-5px'>
-          7
-        </button>
-        <button className='caculator-button light-grey m-5px'>
-          8
-        </button>
-        <button className='caculator-button light-grey m-5px'>
-          9
-        </button>
-        <button className='caculator-button light-blue m-5px'>
-          ÷
-        </button>
-      </div>
-      <div className='flex-row'>
-        <button className='caculator-button light-grey m-5px'>
-          4
-        </button>
-        <button className='caculator-button light-grey m-5px'>
-          5
-        </button>
-        <button className='caculator-button light-grey m-5px'>
-          6
-      </button>
-        <button className='caculator-button light-blue m-5px'>
-          ×
-        </button>
-      </div>
-      <div className='flex-row'>
-        <button className='caculator-button light-grey m-5px'>
-          1
-    </button>
-        <button className='caculator-button light-grey m-5px'>
-          2
-    </button>
-        <button className='caculator-button light-grey m-5px'>
-          3
-    </button>
-        <button className='caculator-button light-blue m-5px'>
-          -
-</button>
-      </div>
-      <div className='flex-row'>
-        <button className='caculator-button light-grey m-5px'>
-          0
-  </button>
-        <button className='caculator-button light-grey m-5px'>
-          .
-  </button>
-        <button className='caculator-button light-grey m-5px'>
-          =
-  </button>
-        <button className='caculator-button light-blue m-5px'>
-          +
-</button>
+
+
+    <div className='body'>
+    
+   
+    <div className="wrapper">
+      <div className="flex-row">
+        <input className="calculator-input " type="number"></input>
       </div>
 
-      <div className='flex-row'>
-        <button className='caculator-button clear-button light-blue m-5px'>
-          C
-      </button>
+      <div className="number-wrapper">
+        <div className="flex-row ">
+          <button className="light-grey">7</button>
+          <button className="light-grey">8</button>
+          <button className="light-grey">9</button>
+          <button className="light-blue">÷</button>
+        </div>
+        <div className="flex-row">
+          <button className="light-grey">4</button>
+          <button className="light-grey">5</button>
+          <button className="light-grey">6</button>
+          <button className="light-blue">×</button>
+        </div>
+        <div className="flex-row">
+          <button className="light-grey">1</button>
+          <button className="light-grey">2</button>
+          <button className="light-grey">3</button>
+          <button className="light-blue">-</button>
+        </div>
+        <div className="flex-row">
+          <button className="light-grey">0</button>
+          <button className="light-grey">.</button>
+          <button className="light-grey">=</button>
+          <button className="light-blue">+</button>
+        </div>
+        <div className="flex-row">
+          <button className="caculator-button clear-button light-blue">
+            C
+          </button>
+        </div>
+      </div>
       </div>
     </div>
-
   );
 }
 
