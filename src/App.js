b6d1e1ff22492d435894f63a3e4ad13473cac841
VadimSchmitz <di3mpi3@gmail.com>
2020-08-19 22:16:36 +0200

    made a component of calculator

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import React from "react";
 import "./index.css";
-
-
+import Calculator from './Components/Calculator'
 
 function App() {
   return (
     <div className='body'>
-      <div className="calculator">
-        <input className="calculator-input" type="number"></input>
-        <button className="light-grey b7">7</button>
-        <button className="light-grey b8">8</button>
-        <button className="light-grey b9">9</button>
-        <button className="light-blue bdiv">÷</button>
-        <button className="light-grey b4">4</button>
-        <button className="light-grey b5">5</button>
-        <button className="light-grey b6">6</button>
-        <button className="light-blue bx">×</button>
-        <button className="light-grey b1">1</button>
-        <button className="light-grey b2">2</button>
-        <button className="light-grey b3">3</button>
-        <button className="light-blue b-">-</button>
-        <button className="light-grey b0">0</button>
-        <button className="light-grey bdot">.</button>
-        <button className="light-grey beq">=</button>
-        <button className="light-blue bpl">+</button>
-        <button className="light-blue bc">C</button>
-      </div>
+      <Calculator/>
     </div>
   );
 }
 
 export default App;
+
