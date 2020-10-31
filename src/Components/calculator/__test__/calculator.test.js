fbb1533bb7de4604a7b1bf5f38f15b3f24db3e43
Vadim <di3mpi3@gmail.com>
2020-10-31 22:30:23 +0100

    test

--- src/Components/calculator/__test__/calculator.test.js
+++ src/Components/calculator/__test__/calculator.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from "react-dom"
 import Calculator from '../../calculator/Calculator';
 
-//test
+//test2
 it("renders without crashing.", ()=>{
     const div = document.createElement("div");
     ReactDOM.render(<Calculator></Calculator>, div)
